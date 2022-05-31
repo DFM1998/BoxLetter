57edc4819bf9eb66655305eb8ad40e3e447905d1
david.freitas <david.freitas@101-Dev3-Stagiaire.local>
2022-05-31 14:44:44 +0200

    filter functions problem

--- public/js/script.js
+++ public/js/script.js
@@ -308,6 +308,12 @@ $(document).ready(function () {
 
 $(".showHideFilter").click(function () {
     $(".filterFunctions").show();
+    $(".resetDefault").show();
+});
+
+$(".filterFunctions").click(function (e) {
+    console.log("EEEE");
+    e.stopPropagation();
 });
 
 let lang = "";
